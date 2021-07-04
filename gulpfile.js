6ef3478c012992e3cd51fb4e9bf16dccf94d7afb
GunukulaNiharika <niharikagunukula2002@gmail.com>
2021-07-04 17:17:08 +0530

    gulp2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 'use strict';
-var gulp=require('gulp');
-var sass=require('gulp-sass');
-var browserSync=require('browser-sync');
+const { use } = require('browser-sync');
+var gulp=require('gulp'),
+    sass=require('gulp-sass'),
+    browserSync=require('browser-sync'),
+    del=require('del'),
+    uglify=require('gulp-uglify'),
+    usemin=require('gulp-usemin'),
+    rev=require('gulp-rev'),
+    cleanCss=require('gulp-clean-css'),
+    htmlmin=require('gulp-htmlmin'),
+    flatmap=require('gulp-flatmap'),
+    imagemin=require('gulp-imagemin');
 gulp.task('sass',function(){
     return gulp.src('./css/*.scss')
     .pipe(sass().on('error',sass.logError))
@@ -25,4 +34,34 @@ gulp.task('browser-sync',function(){
 });
 gulp.task('default',['browser-sync'],function(){
     gulp.start('sass:watch');
+});
+gulp.task('clean',function(){
+    return del(['dist']);
+});
+gulp.task('copyfonts',function(){
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+gulp.task('imagemin',function(){
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ ptimizationLevel: 3, progressive: true, interlaced: true}))
+    .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('usemin',function(){
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream,file){
+        return stream
+        .pipe(usemin({
+            css:[rev()],
+            html: [function(){return htmlmin({collapseWhitespace: true})}],
+            js: [uglify(),rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(),'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+gulp.task('build',['clean'],function(){
+    gulp.start('copyfonts','imagemin','usemin');
 });
